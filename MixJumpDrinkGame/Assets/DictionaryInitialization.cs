f40a2703f23460b1d0e26752826d04094c562c71
Kris-Dimitrov <74430991+Kris-Dimitrov@users.noreply.github.com>
2020-11-14 14:50:04 +0200

    Shite

---
+++ MixJumpDrinkGame/Assets/DictionaryInitialization.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryInitialization : MonoBehaviour
+{
+    [SerializeField] TextAsset text;
+    public Dictionary<string, string> drinksList;
+    void Start()
+    {
+        string textSt = text.ToString();
+        textSt = textSt.Replace("\n","");
+
+        string[] textAr = textSt.Split(';');
+
+        for (int i = 0; i < textAr.Length; i++)
+        {
+            string[] temp = textAr[i].Split('=');
+            drinksList.Add(temp[0],temp[1]);
+        }
+    }
+
+}
